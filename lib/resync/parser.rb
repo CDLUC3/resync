a1530dfbe6513ee8ddc8d273157fa37d644c5291
David Moles <david.moles@ucop.edu>
2015-04-28 13:23:13 -0700

    Started work on parser

--- lib/resync/parser.rb
+++ lib/resync/parser.rb
@@ -1,19 +1,32 @@
 require 'nokogiri'
 
 module Resync
-  module Parser
+  class Parser
 
-    module_function
+    # ------------------------------
+    # Public methods
 
-    def parse(xml)
+    def self.parse(xml)
       doc = Nokogiri::XML(xml)
-      parse_class = case doc.root.name.downcase
-                    when 'urlset'
-                      Urlset
-                    when 'sitemapindex'
-                      Sitemapindex
-                    end
+      parse_class = get_parse_class(doc)
       parse_class.parse(xml, single: true)
+    end
+
+    # ------------------------------
+    # Private methods
+
+    private
+
+    def self.get_parse_class(doc)
+      root_name = doc.root.name
+      case root_name.downcase
+      when 'urlset'
+        Urlset
+      when 'sitemapindex'
+        Sitemapindex
+      else
+        fail "Unexpected root tag <#{root_name}>"
+      end
     end
   end
 end
--- spec/acceptance/resync/parser_spec.rb
+++ spec/acceptance/resync/parser_spec.rb
@@ -4,13 +4,21 @@ module Resync
   describe Parser do
 
     it 'parses example 1' do
-
-      puts Parser.instance_methods
-
       data = File.read('spec/data/examples/example-1.xml')
       urlset = Parser.parse(data)
       expect(urlset).to be_a(Urlset)
+
+      urls = urlset.url
+      expect(urls.size).to eq(2)
+
+      url0 = urls[0]
+      expect(url0.loc).to eq('http://example.com/res1')
+
+      url1 = urls[1]
+      expect(url1.loc).to eq('http://example.com/res2')
     end
+
+    it 'parses resync elements in example 1'
 
   end
 end
