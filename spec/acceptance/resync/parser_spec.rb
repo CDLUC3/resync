12e214d99af556d9cfa4d25caa0efc69252aeb6f
David Moles <david.moles@ucop.edu>
2015-04-29 16:24:23 -0700

    Add more examples

--- spec/acceptance/resync/parser_spec.rb
+++ spec/acceptance/resync/parser_spec.rb
@@ -56,5 +56,42 @@ module Resync
       expect(ln1.href).to eq(URI('http://mirror.example.com/res2'))
     end
 
+    it 'parses example 3' do
+      data = File.read('spec/data/examples/example-3.xml')
+      urlset = Parser.parse(data)
+
+      urls = urlset.url
+      expect(urls.size).to eq(2)
+      url0 = urls[0]
+      expect(url0.loc).to eq(URI('http://example.com/res2.pdf'))
+      expect(url0.lastmod).to eq(DateTime.new(2013, 1, 2, 13))
+      md0 = url0.md
+      expect(md0).not_to be_nil
+      expect(md0.change).to be(Change::UPDATED)
+
+      url1 = urls[1]
+      expect(url1.loc).to eq(URI('http://example.com/res3.tiff'))
+      expect(url1.lastmod).to eq(DateTime.new(2013, 1, 2, 18))
+      md1 = url1.md
+      expect(md1).not_to be_nil
+      expect(md1).not_to be_nil
+      expect(md1.change).to be(Change::DELETED)
+    end
+
+    it 'parses example 4' do
+      data = File.read('spec/data/examples/example-4.xml')
+      urlset = Parser.parse(data)
+
+      md = urlset.md
+      expect(md.capability).to eq('resourcedump')
+      expect(md.at).to eq(DateTime.new(2013, 1, 3, 9))
+
+      urls = urlset.url
+      expect(urls.size).to eq(1)
+
+      url0 = urls[0]
+      expect(url0.loc).to eq(URI('http://example.com/resourcedump.zip'))
+      expect(url0.lastmod).to eq(DateTime.new(2013,1,3,9))
+    end
   end
 end
