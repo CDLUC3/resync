8c772f8d02dc441236ec00e325d5b329e40322b7
David Moles <david.moles@ucop.edu>
2015-05-05 12:06:31 -0700

    Added specs for TimeNode, and loosened 'all W3C datetime formats' to just the xsd:dateTime valid (full) formats

--- spec/rspec_custom_matchers.rb
+++ spec/rspec_custom_matchers.rb
@@ -39,3 +39,21 @@ RSpec::Matchers.define :be_xml do |expected|
     "expected not to get XML:\n\t#{actual_xml}"
   end
 end
+
+RSpec::Matchers.define :be_time do |expected|
+
+  def to_string(time)
+    time.is_a?(Time) ? time.utc.round(2).iso8601(2) : time.to_s
+  end
+
+  match do |actual|
+    fail "Expected value #{expected} is not a Time" unless expected.is_a?(Time)
+    actual.is_a?(Time) && (to_string(expected) == to_string(actual))
+  end
+
+  failure_message do |actual|
+    expected_str = to_string(expected)
+    actual_str = to_string(actual)
+    "expected time:\n#{expected_str}\n\nbut was:\n#{actual_str}"
+  end
+end
--- spec/unit/resync/xml/timenode_spec.rb
+++ spec/unit/resync/xml/timenode_spec.rb
@@ -2,8 +2,47 @@ require 'spec_helper'
 
 module Resync
   module XML
+
+    class Elem
+      include ::XML::Mapping
+      time_node :time, '@time', default_value: nil
+
+      def self.from_str(time_str)
+        xml_string = "<elem time='#{time_str}'/>"
+        doc = REXML::Document.new(xml_string)
+        self.load_from_xml(doc.root)
+      end
+    end
+
     describe TimeNode do
-      it 'supports all W3C datetime formats' # see http://www.w3.org/TR/NOTE-datetime
+
+      def time(str)
+        Elem.from_str(str).time
+      end
+
+      it 'parses a date with hours, minutes, and seconds' do
+        actual = time('1997-07-16T19:20:30')
+        expected = Time.new(1997, 7, 16, 19, 20, 30)
+        expect(actual).to be_time(expected)
+      end
+
+      it 'parses a date with hours, minutes, seconds, and fractional seconds' do
+        actual = time('1997-07-16T19:20:30.45')
+        expected = Time.new(1997, 7, 16, 19, 20, 30.45)
+        expect(actual).to be_time(expected)
+      end
+
+      it 'parses a UTC "zulu" time (time zone designator "Z")' do
+        actual = time('1997-07-16T19:20:30.45Z')
+        expected = Time.new(1997, 7, 16, 19, 20, 30.45, '+00:00')
+        expect(actual).to be_time(expected)
+      end
+
+      it 'parses a time with a numeric timezone offset' do
+        actual = time('1997-07-16T19:20:30.45+01:30')
+        expected = Time.new(1997, 7, 16, 19, 20, 30.45, '+01:30')
+        expect(actual).to be_time(expected)
+      end
     end
   end
 end
