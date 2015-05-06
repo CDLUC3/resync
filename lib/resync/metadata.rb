92f6e78c98e390b91cfdc820f3b0697d38745c34
David Moles <david.moles@ucop.edu>
2015-05-06 15:48:50 -0700

    Reorganized <md/> attributes; added length to metadata

--- lib/resync/metadata.rb
+++ lib/resync/metadata.rb
@@ -1,4 +1,3 @@
-
 module Resync
   class Metadata
 
@@ -8,14 +7,29 @@ module Resync
     attr_reader :at_time
     attr_reader :from_time
     attr_reader :until_time
+    attr_reader :completed_time
+    attr_reader :modified_time
+
+    attr_reader :length
 
     # ------------------------------------------------------------
     # Initializer
 
-    def initialize(at_time: nil, from_time: nil, until_time: nil)
+    def initialize( # rubocop:disable Metrics/MethodLength, Metrics/ParameterLists
+        at_time: nil,
+        from_time: nil,
+        until_time: nil,
+        completed_time: nil,
+        modified_time: nil,
+        length: nil
+    )
       @at_time = time_or_nil(at_time)
       @from_time = time_or_nil(from_time)
       @until_time = time_or_nil(until_time)
+      @completed_time = time_or_nil(completed_time)
+      @modified_time = time_or_nil(modified_time)
+
+      @length = natural_number_or_nil(length)
     end
 
     # ------------------------------------------------------------
@@ -27,10 +41,13 @@ module Resync
     # Parameter validators
 
     def time_or_nil(time)
-      if time && !time.is_a?(Time)
-        fail ArgumentError, "time #{time} is not a Time"
-      end
+      fail ArgumentError, "time #{time} is not a Time" if time && !time.is_a?(Time)
       time
+    end
+
+    def natural_number_or_nil(value)
+      fail ArgumentError, "value #{value} must be a non-negative integer" if value && (!value.is_a?(Integer) || value < 0)
+      value
     end
 
   end
--- spec/unit/resync/metadata_spec.rb
+++ spec/unit/resync/metadata_spec.rb
@@ -4,49 +4,102 @@ module Resync
   describe Metadata do
     describe '#new' do
 
-      it 'accepts an at timestamp' do
-        at_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
-        metadata = Metadata.new(at_time: at_time)
-        expect(metadata.at_time).to be_time(at_time)
+      describe 'timestamps' do
+
+        it 'accepts an at timestamp' do
+          at_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
+          metadata = Metadata.new(at_time: at_time)
+          expect(metadata.at_time).to be_time(at_time)
+        end
+
+        it 'defaults to nil if no at timestamp is specified' do
+          metadata = Metadata.new
+          expect(metadata.at_time).to be_nil
+        end
+
+        it 'fails if the at timestamp is not a time' do
+          expect { Metadata.new(at_time: '12:45 pm') }.to raise_error(ArgumentError)
+        end
+
+        it 'accepts a from timestamp' do
+          from_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
+          metadata = Metadata.new(from_time: from_time)
+          expect(metadata.from_time).to be_time(from_time)
+        end
+
+        it 'defaults to nil if no from timestamp is specified' do
+          metadata = Metadata.new
+          expect(metadata.from_time).to be_nil
+        end
+
+        it 'fails if the from timestamp is not a time' do
+          expect { Metadata.new(from_time: '12:45 pm') }.to raise_error(ArgumentError)
+        end
+
+        it 'accepts an until timestamp' do
+          until_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
+          metadata = Metadata.new(until_time: until_time)
+          expect(metadata.until_time).to be_time(until_time)
+        end
+
+        it 'defaults to nil if no until timestamp is specified' do
+          metadata = Metadata.new
+          expect(metadata.until_time).to be_nil
+        end
+
+        it 'fails if the until timestamp is not a time' do
+          expect { Metadata.new(until_time: '12:45 pm') }.to raise_error(ArgumentError)
+        end
+
+        it 'accepts a completed timestamp' do
+          completed_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
+          metadata = Metadata.new(completed_time: completed_time)
+          expect(metadata.completed_time).to be_time(completed_time)
+        end
+
+        it 'defaults to nil if no completed timestamp is specified' do
+          metadata = Metadata.new
+          expect(metadata.completed_time).to be_nil
+        end
+
+        it 'fails if the completed timestamp is not a time' do
+          expect { Metadata.new(completed_time: '12:45 pm') }.to raise_error(ArgumentError)
+        end
+
+        it 'accepts a modified timestamp' do
+          modified_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
+          metadata = Metadata.new(modified_time: modified_time)
+          expect(metadata.modified_time).to be_time(modified_time)
+        end
+
+        it 'defaults to nil if no modified timestamp is specified' do
+          metadata = Metadata.new
+          expect(metadata.modified_time).to be_nil
+        end
+
+        it 'fails if the modified timestamp is not a time' do
+          expect { Metadata.new(modified_time: '12:45 pm') }.to raise_error(ArgumentError)
+        end
+
       end
 
-      it 'defaults to nil if no at timestamp is specified' do
-        metadata = Metadata.new
-        expect(metadata.at_time).to be_nil
-      end
+      describe 'length' do
+        it 'accepts a length' do
+          length = 12_345
+          metadata = Metadata.new(length: length)
+          expect(metadata.length).to eq(length)
+        end
 
-      it 'fails if the at timestamp is not a time' do
-        expect { Metadata.new(at_time: '12:45 pm') }.to raise_error(ArgumentError)
-      end
+        it 'defaults to nil if no length is specified' do
+          metadata = Metadata.new
+          expect(metadata.length).to be_nil
+        end
 
-      it 'accepts a from timestamp' do
-        from_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
-        metadata = Metadata.new(from_time: from_time)
-        expect(metadata.from_time).to be_time(from_time)
-      end
-
-      it 'defaults to nil if no from timestamp is specified' do
-        metadata = Metadata.new
-        expect(metadata.from_time).to be_nil
-      end
-
-      it 'fails if the from timestamp is not a time' do
-        expect { Metadata.new(from_time: '12:45 pm') }.to raise_error(ArgumentError)
-      end
-
-      it 'accepts an until timestamp' do
-        until_time = Time.utc(1997, 7, 16, 19, 20, 30.45)
-        metadata = Metadata.new(until_time: until_time)
-        expect(metadata.until_time).to be_time(until_time)
-      end
-
-      it 'defaults to nil if no until timestamp is specified' do
-        metadata = Metadata.new
-        expect(metadata.until_time).to be_nil
-      end
-
-      it 'fails if the until timestamp is not a time' do
-        expect { Metadata.new(until_time: '12:45 pm') }.to raise_error(ArgumentError)
+        it 'fails if length is not a non-negative integer' do
+          expect { Metadata.new(length: -12_345) }.to raise_error(ArgumentError)
+          expect { Metadata.new(length: 123.45) }.to raise_error(ArgumentError)
+          expect { Metadata.new(length: 'I am not a number') }.to raise_error(ArgumentError)
+        end
       end
 
     end
