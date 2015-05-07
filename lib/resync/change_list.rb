73c5b6e4caca7caaf16d335c606fb0db3de78e93
David Moles <david.moles@ucop.edu>
2015-05-07 15:08:41 -0700

    Made changelist sort resources

--- lib/resync/change_list.rb
+++ lib/resync/change_list.rb
@@ -4,7 +4,7 @@ module Resync
     attr_reader :metadata
 
     def initialize(resources: [], metadata: Metadata.new(capability: 'changelist'))
-      @resources = resources
+      @resources = sorted(resources)
       @metadata = metadata_with_correct_capability(metadata)
     end
 
@@ -20,6 +20,19 @@ module Resync
       fail ArgumentError, "#{metadata} does not appear to be metadata" unless metadata.is_a?(Metadata)
       fail ArgumentError, "Wrong capability for ChangeList metadata; expected 'changelist', was '#{metadata.capability}'" unless metadata.capability == 'changelist'
       metadata
+    end
+
+    # ------------------------------
+    # Conversions
+
+    def sorted(resources)
+      resources.sort do |left, right|
+        if left.lastmod && right.lastmod
+          left.lastmod <=> right.lastmod
+        else
+          right.lastmod ? 1 : -1
+        end
+      end
     end
 
   end
--- spec/unit/resync/change_list_spec.rb
+++ spec/unit/resync/change_list_spec.rb
@@ -6,8 +6,6 @@ module Resync
 
       describe 'resources' do
 
-        # TODO: Should a change be modeled as an object distinct from a resource?
-
         it 'accepts a list of resources' do
           resources = [Resource.new(uri: 'http://example.org/'), Resource.new(uri: 'http://example.com/')]
           resource_list = ChangeList.new(resources: resources)
@@ -17,6 +15,25 @@ module Resync
         it 'defaults to an empty list if no resources are specified' do
           resource_list = ChangeList.new
           expect(resource_list.resources).to eq([])
+        end
+
+        it 'sorts resources by lastmod' do
+          resource0 = Resource.new(uri: 'http://example.org', lastmod: Time.utc(1997, 7, 16, 19, 20, 30.45))
+          resource1 = Resource.new(uri: 'http://example.org', lastmod: Time.utc(1998, 7, 16, 19, 20, 30.45))
+          misordered = [resource1, resource0]
+          ordered = [resource0, resource1]
+          change_list = ChangeList.new(resources: misordered)
+          expect(change_list.resources).to eq(ordered)
+        end
+
+        it 'sorts resources with lastmod before resourcese without' do
+          resource0 = Resource.new(uri: 'http://example.org', lastmod: Time.utc(1997, 7, 16, 19, 20, 30.45))
+          resource1 = Resource.new(uri: 'http://example.org', lastmod: Time.utc(1998, 7, 16, 19, 20, 30.45))
+          resource2 = Resource.new(uri: 'http://example.com')
+          misordered = [resource1, resource2, resource0]
+          ordered = [resource0, resource1, resource2]
+          change_list = ChangeList.new(resources: misordered)
+          expect(change_list.resources).to eq(ordered)
         end
       end
 
