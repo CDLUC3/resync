fc1ee14e43f9bf03c32b1c5ced33ff50c81317aa
David Moles <david.moles@ucop.edu>
2015-05-07 10:27:53 -0700

    Started implementing ResourceList

--- lib/resync/resource_list.rb
+++ lib/resync/resource_list.rb
@@ -1,5 +1,26 @@
 module Resync
   class ResourceList
+    attr_reader :resources
+    attr_reader :metadata
+
+    def initialize(resources: [], metadata: Metadata.new(capability: 'resourcelist'))
+      @resources = resources
+      @metadata = metadata_with_correct_capability(metadata)
+    end
+
+    # ------------------------------------------------------------
+    # Private methods
+
+    private
+
+    # ------------------------------
+    # Parameter validators
+
+    def metadata_with_correct_capability(metadata)
+      fail ArgumentError, "#{metadata} does not appear to be metadata" unless metadata.is_a?(Metadata)
+      fail ArgumentError, "Wrong capability for ResourceList metadata; expected 'resourcelist', was '#{metadata.capability}'" unless metadata.capability == 'resourcelist'
+      metadata
+    end
 
   end
 end
--- spec/unit/resync/resource_list_spec.rb
+++ spec/unit/resync/resource_list_spec.rb
@@ -2,6 +2,42 @@ require 'spec_helper'
 
 module Resync
   describe ResourceList do
-    it 'is implemented'
+
+    describe '#new' do
+
+      describe 'resources' do
+        it 'accepts a list of resources' do
+          resources = [Resource.new(uri: 'http://example.org/'), Resource.new(uri: 'http://example.com/')]
+          resource_list = ResourceList.new(resources: resources)
+          expect(resource_list.resources).to eq(resources)
+        end
+
+        it 'defaults to an empty list if no resources are specified' do
+          resource_list = ResourceList.new
+          expect(resource_list.resources).to eq([])
+        end
+      end
+
+      describe 'metadata' do
+        it 'accepts metadata' do
+          metadata = Metadata.new(capability: 'resourcelist')
+          resource_list = ResourceList.new(metadata: metadata)
+          expect(resource_list.metadata).to eq(metadata)
+        end
+
+        it 'defaults (otherwise empty) metadata with capability "resourcelist" if no metadata specified' do
+          resource_list = ResourceList.new
+          metadata = resource_list.metadata
+          expect(metadata.capability).to eq('resourcelist')
+        end
+
+        it 'fails if metadata does not have capability "resourcelist"' do
+          expect { ResourceList.new(metadata: Metadata.new) }.to raise_error(ArgumentError)
+          expect { ResourceList.new(metadata: Metadata.new(capability: 'changelist')) }.to raise_error(ArgumentError)
+        end
+      end
+
+    end
+
   end
 end
