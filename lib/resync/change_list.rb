6fa6884ad82c9d76f37ac5d814fd74ed46322add
David Moles <david.moles@ucop.edu>
2015-05-07 10:45:57 -0700

    Initial work on ChangeList

--- lib/resync/change_list.rb
+++ lib/resync/change_list.rb
@@ -1,4 +1,26 @@
 module Resync
   class ChangeList
+    attr_reader :resources
+    attr_reader :metadata
+
+    def initialize(resources: [], metadata: Metadata.new(capability: 'changelist'))
+      @resources = resources
+      @metadata = metadata_with_correct_capability(metadata)
+    end
+
+    # ------------------------------------------------------------
+    # Private methods
+
+    private
+
+    # ------------------------------
+    # Parameter validators
+
+    def metadata_with_correct_capability(metadata)
+      fail ArgumentError, "#{metadata} does not appear to be metadata" unless metadata.is_a?(Metadata)
+      fail ArgumentError, "Wrong capability for ChangeList metadata; expected 'changelist', was '#{metadata.capability}'" unless metadata.capability == 'changelist'
+      metadata
+    end
+
   end
 end
--- spec/unit/resync/change_list_spec.rb
+++ spec/unit/resync/change_list_spec.rb
@@ -2,5 +2,45 @@ require 'spec_helper'
 
 module Resync
   describe ChangeList do
+    describe '#new' do
+
+      describe 'resources' do
+
+        # TODO: Should a change be modeled as an object distinct from a resource?
+
+        it 'accepts a list of resources' do
+          resources = [Resource.new(uri: 'http://example.org/'), Resource.new(uri: 'http://example.com/')]
+          resource_list = ChangeList.new(resources: resources)
+          expect(resource_list.resources).to eq(resources)
+        end
+
+        it 'defaults to an empty list if no resources are specified' do
+          resource_list = ChangeList.new
+          expect(resource_list.resources).to eq([])
+        end
+      end
+
+      describe 'metadata' do
+        it 'accepts metadata' do
+          metadata = Metadata.new(capability: 'changelist')
+          resource_list = ChangeList.new(metadata: metadata)
+          expect(resource_list.metadata).to eq(metadata)
+        end
+
+        it 'defaults (otherwise empty) metadata with capability "changelist" if no metadata specified' do
+          resource_list = ChangeList.new
+          metadata = resource_list.metadata
+          expect(metadata.capability).to eq('changelist')
+        end
+
+        it 'fails if metadata does not have capability "changelist"' do
+          expect { ChangeList.new(metadata: Metadata.new) }.to raise_error(ArgumentError)
+          expect { ChangeList.new(metadata: Metadata.new(capability: 'resourcelist')) }.to raise_error(ArgumentError)
+        end
+
+        it 'requires from and until times (?)'
+      end
+
+    end
   end
 end
