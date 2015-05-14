14dcbf6afc182d4142377bb74fb49089e4b04480
David Moles <david.moles@ucop.edu>
2015-05-14 16:03:48 -0700

    Added from_xml to ChangeDumpManifest

--- lib/resync/change_dump_manifest.rb
+++ lib/resync/change_dump_manifest.rb
@@ -1,7 +1,10 @@
 require_relative 'shared/sorted_resource_list'
+require_relative 'xml'
 
 module Resync
   class ChangeDumpManifest < SortedResourceList
+    include XML::Convertible
+    XML_TYPE = XML::Urlset
     CAPABILITY = 'changedump-manifest'
   end
 end
