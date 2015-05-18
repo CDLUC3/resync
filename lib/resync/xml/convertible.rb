486efa7c2ef3cbbace3aeb1a60d4ad2c3011104b
David Moles <david.moles@ucop.edu>
2015-05-18 13:28:25 -0700

    Cleaned up code

--- lib/resync/xml/convertible.rb
+++ lib/resync/xml/convertible.rb
@@ -10,35 +10,6 @@ module Resync
 
       def self.included(base)
         base.extend XMLConversions
-
-        # Overrides +::XML::Mapping::ClassMethods.load_from_xml+ in order to
-        # fall back to :_default mapping when an unknown mapping is specified;
-        # this allows alternate mappings to include sub-elements that don't
-        # know anything about the specified alternate mapping
-        def base.load_from_xml(xml, options={ mapping: :_default })
-          mapping = options[:mapping]
-          unless xml_mapping_nodes_hash.has_key?(mapping)
-            raise(::XML::MappingError, "undefined mapping: #{options[:mapping].inspect} for #{self}, and no :_default mapping found") unless xml_mapping_nodes_hash.has_key?(:_default)
-            mapping = :_default
-          end
-          begin
-            obj = self.new
-          rescue ArgumentError
-            obj = self.allocate
-          end
-          obj.initialize_xml_mapping mapping: mapping
-          obj.fill_from_xml xml, mapping: mapping
-          obj
-        end
-
-        # def to_xml(options:{ mapping: :_default })
-        #   xml = save_to_xml(options)
-        #   formatter = REXML::Formatters::Pretty.new(2)
-        #   formatter.compact = true
-        #   out = StringIO.new
-        #   formatter.write(xml, out)
-        #   out.string
-        # end
       end
 
       # Defines methods that will become class methods on those
@@ -47,6 +18,28 @@ module Resync
         def from_xml(xml)
           xml = ::Resync::XML.element(xml)
           load_from_xml(xml)
+        end
+
+        # Overrides +::XML::Mapping::ClassMethods.load_from_xml+ in order to
+        # fall back to :_default mapping when an unknown mapping is specified;
+        # this allows alternate mappings to include sub-elements that don't
+        # know anything about the specified alternate mapping
+        def load_from_xml(xml, options = { mapping: :_default })
+          mapping = valid_mapping(options[:mapping])
+          begin
+            obj = new
+          rescue ArgumentError
+            obj = allocate
+          end
+          obj.initialize_xml_mapping mapping: mapping
+          obj.fill_from_xml xml, mapping: mapping
+          obj
+        end
+
+        def valid_mapping(mapping)
+          return mapping if xml_mapping_nodes_hash.key?(mapping)
+          fail(::XML::MappingError, "undefined mapping: #{options[:mapping].inspect} for #{self}, and no :_default mapping found") unless xml_mapping_nodes_hash.key?(:_default)
+          :_default
         end
       end
 
