ca49fd67d6906f2a9c6dcf178380332b705e12b5
David Moles <david.moles@ucop.edu>
2015-05-19 11:47:15 -0700

    Hack in namespace declarations very hackily

--- lib/resync/shared/link_collection.rb
+++ lib/resync/shared/link_collection.rb
@@ -2,18 +2,35 @@ require_relative '../link'
 require_relative '../metadata'
 
 module Resync
+
   class LinkCollection
     include ::XML::Mapping
 
     # ------------------------------------------------------------
+    # Constants
+
+    # TODO: Clean this up and make it a method or something
+    PREFIXIZER = Proc.new do |obj, xml, default_obj_to_xml|
+      default_obj_to_xml.call(obj, xml)
+      xml.each_element do |e|
+        case e.name
+        when 'ln'
+          e.name = 'rs:ln'
+        when 'md'
+          e.name = 'rs:md'
+        end
+      end
+    end
+
+    # ------------------------------------------------------------
     # Attributes
 
-    array_node :links, 'ln', class: Link, default_value: []
-    object_node :metadata, 'md', class: Metadata, default_value: nil
+    array_node :links, 'ln', class: Link, default_value: [], writer: PREFIXIZER
+    object_node :metadata, 'md', class: Metadata, default_value: nil, writer: PREFIXIZER
 
     use_mapping :sitemapindex
-    array_node :links, 'ln', class: Link, default_value: []
-    object_node :metadata, 'md', class: Metadata, default_value: nil
+    array_node :links, 'ln', class: Link, default_value: [], writer: PREFIXIZER
+    object_node :metadata, 'md', class: Metadata, default_value: nil, writer: PREFIXIZER
 
     # ------------------------------------------------------------
     # Initializer
