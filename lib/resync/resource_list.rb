4fb3455f678aba133810212d19de43e4c0fd5bf9
David Moles <david.moles@ucop.edu>
2015-05-21 16:58:34 -0700

    Fixed comments

--- lib/resync/resource_list.rb
+++ lib/resync/resource_list.rb
@@ -2,7 +2,7 @@ require_relative 'shared/base_resource_list'
 require_relative 'xml'
 
 module Resync
-  # A resource dump. See section 10.1,
+  # A resource list. See section 10.1,
   # "{http://www.openarchives.org/rs/1.0/resourcesync#ResourceList Resource List}",
   # in the ResourceSync specification.
   class ResourceList < BaseResourceList
